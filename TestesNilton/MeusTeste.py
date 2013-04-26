e7019b80e62a9f7be998b6f15b29217822a3888b
NiltonDuarte <nilton.gduarte@gmail.com>
2013-04-26 03:13:11 -0300

    Update MeusTeste.py

--- TestesNilton/MeusTeste.py
+++ TestesNilton/MeusTeste.py
@@ -1,6 +1,6 @@
 from torre import *
 
 torre1 = Torre()
-torre1.definirParametros([3 0,40,10,40,10,40,10,30])
+torre1.definirParametros([30,40,10,40,10,40,10,30])
 torre1.iniciarTorre()
 print torre1.projetil.massa

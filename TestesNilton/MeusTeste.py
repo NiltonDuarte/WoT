828a51ad89159c610faaa4440b85de07bbfe011c
Lygia Marina <lygia.marina@gmail.com>
2013-05-06 22:51:14 -0300

    Função Aleatória sem recursão

--- TestesNilton/MeusTeste.py
+++ TestesNilton/MeusTeste.py
@@ -2,6 +2,7 @@ from torre import *
 
 #DAFUQ GITHUB???
 
+
 torre1 = Torre()
 torre1.definirParametros([90,150,30,180,30,120,10,100])
 torre1.iniciarTorre()
--- TestesNilton/funcoesComuns.py
+++ TestesNilton/funcoesComuns.py
@@ -3,31 +3,32 @@
 from random import randint
 from math import fabs
 
-VALUE = 0
+VALOR = 0
 MAX = 1
 MIN = 2
+NUM_IT = 100
 
 
-def iniciarAtributosAleatoriosOld(lista, pontos):
+def iniciarAtributosAleatoriosOld0(lista, pontos):
         flagRecursao = False
         while pontos > 0:
             indiceAleatorio = randint(0, len(lista)-1)
-            if ( lista[indiceAleatorio][VALUE] < lista[indiceAleatorio][MAX]):
-                lista[indiceAleatorio][VALUE] += 1
+            if ( lista[indiceAleatorio][VALOR] < lista[indiceAleatorio][MAX]):
+                lista[indiceAleatorio][VALOR] += 1
                 pontos -= 1
 
-        for index in range(len(lista)):
-            verificacaoIntervalo = lista[index][VALUE] - lista[index][MIN]
+        for indice in range(len(lista)):
+            verificacaoIntervalo = lista[indice][VALOR] - lista[indice][MIN]
             print lista
             
             if verificacaoIntervalo < 0:
                 flagRecursao = True
-                listaTemporaria = lista[0:index] + lista[index+1:]
-                for indexIF in range(len(listaTemporaria)):
-                    condicao = 1 if listaTemporaria[indexIF][VALUE] > listaTemporaria[indexIF][MIN] else 0
-                    listaTemporaria[indexIF][VALUE] -= condicao
+                listaTemporaria = lista[0:indice] + lista[indice+1:]
+                for indiceIF in range(len(listaTemporaria)):
+                    condicao = 1 if listaTemporaria[indiceIF][VALOR] > listaTemporaria[indiceIF][MIN] else 0
+                    listaTemporaria[indiceIF][VALOR] -= condicao
                     pontos += condicao
-                lista[index][VALUE] += pontos if (-verificacaoIntervalo > pontos) else -verificacaoIntervalo
+                lista[indice][VALOR] += pontos if (-verificacaoIntervalo > pontos) else -verificacaoIntervalo
                 pontos = 0 if (-verificacaoIntervalo > pontos) else (pontos + verificacaoIntervalo)
                 
         if flagRecursao: iniciarAtributosAleatorios(lista,pontos)
@@ -35,13 +36,30 @@ def iniciarAtributosAleatoriosOld(lista, pontos):
         print lista
 
 
-def iniciarAtributosAleatorios(lista, pontos):
+def iniciarAtributosAleatoriosOld1(lista, pontos):
     pontosAtuais = pontos
-    for index in range(len(lista)):
-        lista[index][VALUE] = randint(lista[index][MIN], lista[index][MAX])
-        pontosAtuais -= lista[index][VALUE]
+    for indice in range(len(lista)):
+        lista[indice][VALOR] = randint(lista[indice][MIN], lista[indice][MAX])
+        pontosAtuais -= lista[indice][VALOR]
     if fabs(pontosAtuais) > pontos/10:
         iniciarAtributosAleatorios(lista,pontos)
     print lista, pontosAtuais
+
+def iniciarAtributosAleatorios(lista, pontos):
+        listaAtual = [x[:] for x in lista]
+        pontosAtuais = 2*pontos
+        dist = abs(pontosAtuais - pontos)
+        for i in range(NUM_IT):
+                pontosAtuais = 0
+                for indice in range(len(listaAtual)):
+                        listaAtual[indice][VALOR] = randint(listaAtual[indice][MIN], listaAtual[indice][MAX])
+                        pontosAtuais += listaAtual[indice][VALOR]
+                dist_atual = abs(pontosAtuais - pontos)
+                if dist_atual < dist:
+                        for indice in range(len(lista)):
+                                lista[indice][VALOR] = listaAtual[indice][VALOR]
+                        dist = dist_atual
+                        pontosFinais = pontosAtuais
+        print lista, pontosFinais
     
     

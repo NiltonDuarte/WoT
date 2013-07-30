c6f3656515e98766eb8e4a4ff04561953925db04
Rodolfombc <rodolfombc@gmail.com>
2013-07-30 02:02:06 -0300

    packpanda updated

---
+++ packpanda-TMP/game/source/Sound.py
@@ -0,0 +1,25 @@
+from pandaImports import *
+
+class Sound(DirectObject):
+	'''This class handles the songs and sound effects of the game
+	'''
+	def __init__(self, sound):
+		#Loading the sound
+		self.sound = base.loader.loadSfx(sound)  #returns type AudioSound
+
+	def play(self):
+		self.sound.play()
+	
+	def stop(self):
+		self.sound.stop()
+	
+	def setVolume(self, volume):
+		self.sound.setVolume(volume)
+		
+	def getSoundStatus(self):
+		return self.sound.status()
+		
+	def setLoop(self, isLoop):
+		self.sound.setLoop(isLoop)
+		
+
